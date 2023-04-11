f95e48f60dd789e3f6b1e518fcbfb2cb983f828e
Aman07a <aman16.aa17@gmail.com>
2023-04-11 09:44:43 +0200

    Interior Page Template

--- header.php
+++ header.php
@@ -9,15 +9,14 @@
     <header class="site-header">
         <div class="container">
             <h1 class="school-logo-text float-left">
-                <a href="#"><strong>Fictional</strong> University</a>
+                <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
             </h1>
-            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search"
-                    aria-hidden="true"></i></span>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
                 <nav class="main-navigation">
-                    <ul>
-                        <li><a href="#">About Us</a></li>
+                    <ul class="min-list group">
+                        <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                         <li><a href="#">Programs</a></li>
                         <li><a href="#">Events</a></li>
                         <li><a href="#">Campuses</a></li>
@@ -26,9 +25,8 @@
                 </nav>
                 <div class="site-header__util">
                     <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-                    <span class="search-trigger js-search-trigger"><i class="fa fa-search"
-                            aria-hidden="true"></i></span>
+                    <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
                 </div>
             </div>
         </div>
