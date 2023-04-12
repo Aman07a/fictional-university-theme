c10ddcc879ac927a8461a35c24ba5ac47a0f6097
Aman07a <aman16.aa17@gmail.com>
2023-04-12 18:48:05 +0200

    Featured Image (Post Thumbnail)

--- single-program.php
+++ single-program.php
@@ -18,7 +18,8 @@ while (have_posts()) {
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
                 <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link("program"); ?>">
-                    <i class="fa fa-home" aria-hidden="true"></i> All Programs</a>
+                    <i class="fa fa-home" aria-hidden="true"></i> All Programs
+                </a>
                 <span class="metabox__main"><?php the_title(); ?></span>
             </p>
         </div>
@@ -43,12 +44,17 @@ while (have_posts()) {
             echo '<hr class="section-break">';
             echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
 
+            echo '<ul class="professor-cards">';
             while ($relatedProfessors->have_posts()) {
                 $relatedProfessors->the_post(); ?>
-                <li>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <li class="professor-card__list-item">
+                    <a class="professor-card" href="<?php the_permalink(); ?>">
+                        <img class="professor-card__image" src="<?php the_post_thumbnail_url() ?>">
+                        <span class="professor-card__name"><?php the_title(); ?></span>
+                    </a>
                 </li>
             <?php }
+            echo "</ul>";
         }
 
         wp_reset_postdata();
