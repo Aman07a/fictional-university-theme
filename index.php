acb1693279f69a72d0a04e2f1d30e2e7744c0065
Aman07a <aman16.aa17@gmail.com>
2023-04-14 18:14:08 +0200

    Traditional WordPress Searching (Part 2)

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ pageBanner(array(
     while (have_posts()) {
         the_post(); ?>
         <div class="post-item">
-            <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2 class="headline headline--medium headline--post-title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h2>
 
             <div class="metabox">
                 <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in
@@ -21,7 +23,9 @@ pageBanner(array(
 
             <div class="generic-content">
                 <?php the_excerpt(); ?>
-                <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+                <p>
+                    <a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a>
+                </p>
             </div>
 
         </div>
