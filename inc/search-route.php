1a3714801c1babac186c23d1c7607d733a7ff18e
Aman07a <aman16.aa17@gmail.com>
2023-04-14 11:59:06 +0200

    jQuery Free Live Search

--- inc/search-route.php
+++ inc/search-route.php
@@ -139,6 +139,7 @@ function universitySearchResults($data)
         }
 
         $results["professors"] = array_values(array_unique($results["professors"], SORT_REGULAR));
+        $results["campuses"] = array_values(array_unique($results["campuses"], SORT_REGULAR));
         $results["events"] = array_values(array_unique($results["events"], SORT_REGULAR));
     }
 
