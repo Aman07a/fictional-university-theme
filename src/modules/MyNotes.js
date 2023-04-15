ce154db9d2552ee626b9a7122fa9b6263ae0a285
Aman07a <aman16.aa17@gmail.com>
2023-04-15 09:04:54 +0200

    "My Notes" Front-end Part 2

--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -6,12 +6,26 @@ class MyNotes {
   }
 
   events() {
-    $(".delete-note").on("click", this.deleteNode);
+    $(".delete-note").on("click", this.deleteNote);
   }
 
   // Methods will go here
-  deleteNode() {
-    alert("you clicked delete");
+  deleteNote() {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
+      url: universityData.root_url + "/wp-json/wp/v2/note/117",
+      type: "DELETE",
+      success: (response) => {
+        console.log("Congrats");
+        console.log(response);
+      },
+      error: (response) => {
+        console.log("Sorry");
+        console.log(response);
+      },
+    });
   }
 }
 
